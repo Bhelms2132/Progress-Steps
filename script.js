3c7528dbeba3a2560378d2c5b2b43372faacd800
Brian Helms <bhelms2132@gmail.com>
2021-09-03 21:00:09 -0400

    Eventlistener working properly not allowing progress to exed 4th step

--- script.js
+++ script.js
@@ -3,4 +3,15 @@ const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const circles = document.querySelectorAll('.circle')
 
+//--Represents an index
 let currentActive = 1
+
+//--Event Listner
+next.addEventListener('click', () => {
+    currentActive++
+
+    //--Prevents clicks from exceeding 4 step limit
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
+})
